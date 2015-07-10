19c851e75f978a1032cf5db5ac825ebede3fb287
Will Fleming <will@mintdigital.com>
2015-07-09 21:25:36 -0400

    initial commit: mostly empty app
    just a rough proof that i can write rules to a file not exclusive to the
    extension, then load them. i.e. change block rules over time.

---
+++ pblock-extension/ActionRequestHandler.swift
@@ -0,0 +1,29 @@
+//
+//  ActionRequestHandler.swift
+//  pblock-extension
+//
+//  Created by Will Fleming on 7/9/15.
+//  Copyright © 2015 Will Fleming. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+  func beginRequestWithExtensionContext(context: NSExtensionContext) {
+    let item = NSExtensionItem()
+
+    let fm = NSFileManager.defaultManager()
+    let dirURL = fm.containerURLForSecurityApplicationGroupIdentifier("group.com.wfleming.pblock")!
+    let filePathURL = dirURL.URLByAppendingPathComponent("blockRules.json")
+
+    if fm.fileExistsAtPath(filePathURL.path!) {
+      let attachment = NSItemProvider(contentsOfURL: filePathURL)!
+      item.attachments = [attachment]
+    }
+
+    context.completeRequestReturningItems([item], completionHandler: nil);
+  }
+  
+}
---
+++ pblockUITests/pblockUITests.swift
@@ -0,0 +1,34 @@
+//
+//  pblockUITests.swift
+//  pblockUITests
+//
+//  Created by Will Fleming on 7/9/15.
+//  Copyright © 2015 Will Fleming. All rights reserved.
+//
+
+import XCTest
+
+class pblockUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
