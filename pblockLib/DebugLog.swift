3670022f3b4551b39157b5d73ad0ebbf3f4c07bf
Will Fleming <will@mintdigital.com>
2015-08-16 14:36:23 -0400

    update debug log for recent swift changes

--- pblockLib/DebugLog.swift
+++ pblockLib/DebugLog.swift
@@ -14,7 +14,7 @@ Adapted from https://gist.github.com/Abizern/a81f31a75e1ad98ff80d
 Prints the filename, function name, line number and textual representation of `object` and a newline character into
 the standard output if the build setting for "Other Swift Flags" defines `-D DEBUG`.
 
-Only the first parameter needs to be passed to this funtion.
+Only the first parameter needs to be passed to this function.
 
 The textual representation is obtained from the `message` using its protocol conformances, in the following
 order of preference: `Streamable`, `Printable`, `DebugPrintable`. Do not overload this function for your type.
@@ -25,9 +25,9 @@ Instead, adopt one of the protocols mentioned above.
 :param: function The name of the function, defaults to the function within which the call is made.
 :param: line     The line number, defaults to the line number within the file that the call is made.
 */
-func dlog<T>(@autoclosure message: () -> T, _ file: String = __FILE__, _ function: String = __FUNCTION__, _ line: Int = __LINE__) {
+func dlog<T>(@autoclosure message: () -> T, _ file: NSString = __FILE__, _ function: String = __FUNCTION__, _ line: Int = __LINE__) {
   #if DEBUG
-    let file = file.lastPathComponent.stringByDeletingPathExtension
+    let file = (file.lastPathComponent as NSString).stringByDeletingPathExtension
 
     print("\(file).\(function)[\(line)]: \(message())\n")
   #endif
